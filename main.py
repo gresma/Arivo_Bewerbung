c1a185cf55079633419f1cae7be238e61ff07c8b
Marco Greschonig <marco.greschonig@student.tugraz.at>
2023-12-23 18:05:49 +0100

    Changed error pop up

--- main.py
+++ main.py
@@ -2,77 +2,107 @@ import PySimpleGUI as gui
 
 
 def is_valid_license_plate(license_plate=''):
-  if license_plate == "valid":
-    return True
-  return False
+    if license_plate == "valid":
+        return True
+    return False
 
 def is_valid_date(date=''):
-  if date == "valid":
-    return True
-  return False
+    if date == "valid":
+        return True
+    return False
 
 def is_valid_card(card=''):
-  if card == "valid":
-    return True
-  return False
+    if card == "valid":
+        return True
+    return False
 
 
 def search_reciept(license_plate, date, card_numbers):
-  return
+    return
+
+def gui_loop(window: gui.Window) -> list[any]:
+
+    event, values = window.read()
+    retval = []
+
+    if event == gui.WINDOW_CLOSED or event == 'Exit':
+        retval.append(gui.WINDOW_CLOSED)
+        return retval
+    if event == 'Search':
+        license_plate = values['license_plate']
+        date = values['date']
+        card = values['card']
+        invalid = []
+
+        print(license_plate + date + card)
+
+        if not is_valid_license_plate(license_plate):
+            window['license_plate'].update(value='', background_color='salmon')
+            invalid.append("License Plate")
+        else:
+            window['license_plate'].update(background_color='white')
+            retval.append(license_plate)
+
+        if not is_valid_date(date):
+            window['date'].update(background_color='salmon')
+            invalid.append("Date")
+        else:
+            window['date'].update(background_color='white')
+            retval.append(date)
+
+
+        if not is_valid_card(card):
+            window['card'].update(background_color='salmon')
+            invalid.append("Card Number")
+        else:
+            window['card'].update(background_color='white')
+            retval.append(card)
+
+
+
+        if invalid:
+            error_message = "Your input for:\n"
+            match len(invalid):
+                case 1:
+                    error_message += invalid[0] + " is invalid."
+                case 2:
+                    error_message += invalid[0] + " and " + invalid[1] + " are invalid."
+                case 3:
+                    error_message += invalid[0] + ", " + invalid[1] + " and " + invalid[2] + " are invalid."
+                case _:
+                    error_message += "Something went wrong."
+            error_message +=  "\nDouble check your Input's Format."
+            gui.popup_error(error_message)
+        else:
+            return license_plate, date, card
+            
+
+
+
 
 
 def main():
 
-  input_and_options = [[gui.Text("License Plate:"), gui.Push(), gui.InputText(key='license_plate')],
-                       [gui.Text("Date of Payment:"), gui.Push(), gui.InputText(key='date')],
-                       [gui.Text("Last 4 digits of your Card:"), gui.Push(), gui.InputText(key='card')],
-                       [gui.Push(), gui.Button("Search"), gui.Button("Exit")]
+    input_and_options = [[gui.Text("License Plate:"), gui.Push(), gui.InputText("License Plate", key='license_plate')],
+                        [gui.Text("Date of Payment:"), gui.Push(), gui.InputText(key='date')],
+                        [gui.Text("Last 4 digits of your Card:"), gui.Push(), gui.InputText(key='card')],
+                        [gui.Push(), gui.Button("Search"), gui.Button("Exit")]
             ]
 
-  search_results = [[gui.Button("Click me! I don't do anything!")]]
+    search_results = [[gui.Button("Click me! I crash everything!")]]
 
-  layout = [[gui.Column(input_and_options),
-            gui.VSeperator(),
-            gui.Column(search_results)]
-            ]
+    layout = [[gui.Column(input_and_options),
+                gui.VSeperator(),
+                gui.Column(search_results)]
+                ]
 
-  window = gui.Window("Arivo Reciept Search Engine", layout, size=(1000, 800))
-
-  while True:
-
-
-    event, values = window.read()
-
-    if event == gui.WINDOW_CLOSED or event == 'Exit':
-      break
-    if event == 'Search':
-      print(values)
-      license_plate = values['license_plate']
-      date = values['date']
-      card = values['card']
-
-      print(license_plate + date + card)
-
-      if not is_valid_license_plate(license_plate):
-        window['license_plate'].update(value='', background_color='salmon')
-        gui.popup_error('Invalid License Plate. Please enter a valid value')
-      else:
-        window['license_plate'].update(background_color='white')
-
-      if not is_valid_date(date):
-        window['date'].update(background_color='salmon')
-        gui.popup_error('Invalid Date. Please enter a valid value')
-      else:
-        window['date'].update(background_color='white')
-
-
-      if not is_valid_card(card):
-        window['card'].update(background_color='salmon')
-        gui.popup_error('Invalid card number. Please enter a valid value')
-      else:
-        window['card'].update(background_color='white')
-
-
+    window = gui.Window("Arivo Reciept Search Engine", layout, size=(1000, 800))
+    retval =[]
+    while True:
+        retval = gui_loop(window)
+        if(retval[0] == gui.WINDOW_CLOSED):
+            break
+        print(retval)
 
 
 
